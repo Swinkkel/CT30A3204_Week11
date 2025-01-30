bc5045060a9c7219ae93ea0dff7e77afe651c23d
Tomi Helin <tomi.helin@student.lut.fi>
2025-01-30 18:46:22 +0200

    useFetch to get jokes

---
+++ src/components/FrontPage.tsx
@@ -0,0 +1,42 @@
+
+import {Button} from '@mui/material'
+import useFetch from './useFetch'
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader'
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+interface IJoke {
+    type: string
+    setup: string
+    punchline: string
+    id: number
+}
+
+const FrontPage = () => {
+    const url: string = 'https://official-joke-api.appspot.com/random_joke'
+    const {data, loading, error} = useFetch(url)
+
+    const convertedData: IJoke = data as IJoke
+
+    if (convertedData) console.log(convertedData.punchline)
+
+    return (
+        <div>
+            <h1>Welcome to Joke Generator</h1>
+            {loading && <p>Loading joke...</p>}
+            {error && <p>{error}</p>}
+            {convertedData && (
+                <div>
+                    <Card key={convertedData.id} variant="outlined">
+                        <CardHeader title={convertedData.setup} />
+                        <CardContent><Typography>{convertedData.punchline}</Typography></CardContent>
+                    </Card>
+                </div>
+            )}
+            <Button onClick={()=>window.location.reload()}>GET JOKE</Button>
+        </div>
+    )
+}
+
+export default FrontPage
